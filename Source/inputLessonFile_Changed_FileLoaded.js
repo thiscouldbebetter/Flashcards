085873db5aa43dc25cd175be446c7d15764c8b2c
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2018-10-28 10:41:03 -0400

    Moved ui event handlers from their own files nto  the html file.

--- Source/inputLessonFile_Changed_FileLoaded.js
+++
@@ -1,7 +0,0 @@
-
-function inputLessonFile_Changed_FileLoaded(event)
-{
-	var fileContents = event.target.result;  
-  var textareaLesson = document.getElementById("textareaLesson");
-	textareaLesson.value = fileContents;
-}
